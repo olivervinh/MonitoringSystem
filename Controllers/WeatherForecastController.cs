c6a368188afa5946a58c85c91a41b34814fdb39d
olivervinh@gmail.com <olivervinh@gmail.com>
2023-11-13 09:14:38 +0700

    Update.

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -24,7 +24,7 @@ namespace WebAPI.Controllers
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 85),
+                TemperatureC = Random.Shared.Next(-20, 100),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
